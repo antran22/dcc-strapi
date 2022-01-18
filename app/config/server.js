cc5aa9f2b5e43b7c9d25fd7bfcc6ce14425afda5
An <trancongvietan22@gmail.com>
2022-01-18 06:56:14 +0000

    initial_config

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bb89e3166672c372204183d20468f2e6'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b411c888-f553-46a1-975e-36984bc50465'
+};
